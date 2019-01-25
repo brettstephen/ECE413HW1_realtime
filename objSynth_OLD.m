c360a91687bfbb68f6de18553345a7b59e078807
Hoerning <thoernin@Tims-MacBook-Pro-2.local>
2019-01-24 22:34:53 -0500

    Working with system objects!  At least objSynth is.  Need to work on rest.

---
+++ objSynth_OLD.m
@@ -0,0 +1,67 @@
+
+% First draft a Synthesizer Object
+
+%classdef objSynthSine < matlab.System
+classdef objSynth_OLD < handle
+    properties
+        % Defaults
+        oscConfig                   = confOsc;
+        constants                   = confConstants;
+    end
+    properties
+        %properties (GetAccess = private)
+        % Private members
+        currentTime;
+        arrayNotes                  = objNote.empty(8,0);
+        arraySynths                 = objOsc.empty(8,0);
+    end
+    
+    methods
+        function obj = objSynth_OLD(varargin)
+            if nargin > 0
+                % Add the objects to the input
+                obj.currentTime=0;
+                
+                if nargin >= 3
+                    obj.constants=varargin{3};
+                end
+                if nargin >= 2
+                    obj.oscConfig=varargin{2};
+                end
+                obj.arrayNotes=varargin{1}.arrayNotes;
+            end
+            
+            for cntNote=1:length(obj.arrayNotes)
+                obj.arraySynths(cntNote)=objOsc(obj.arrayNotes(cntNote),obj.oscConfig,obj.constants);
+            end
+            
+        end
+        
+    end
+    %methods (Access = protected)
+    %function audioAccumulator = stepImpl(obj)
+    
+    methods
+        function audioAccumulator = advance(obj)
+            
+            audioAccumulator=[];
+            for cntNote = 1:length(obj.arrayNotes)
+                
+                audio = obj.arraySynths(cntNote).advance;
+                %audio = step(obj.arraySynths(cntNote));
+                if ~isempty(audio)
+                    if isempty(audioAccumulator)
+                        audioAccumulator=audio;
+                    else
+                        audioAccumulator=audioAccumulator+audio;
+                    end
+                end
+                
+            end
+        end
+    end
+end
+
+
+
+
